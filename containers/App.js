5db1edfd0e60c3745397f69bec6e0c8726ffee7a
LRydeard <luke.rydeard@informed.com>
2018-08-11 20:45:07 +0100

    Initial

---
+++ containers/App.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    render () {
+        return <p>Hello World! I'm not a fan of the classics, though.</p>
+    }
+}
